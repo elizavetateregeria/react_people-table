bf0bbcf187c8f534660cc844941911fbb5827cd9
felsi13 <13felsi@gmail.com>
2019-07-17 15:20:54 +0300

    Updates

---
+++ precache-manifest.489d69f3b4ce81fff072e7b09197a66e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55312e6a6a782370d18141f5fbe2cc5b",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "4bba454c93533744ed05",
+    "url": "/react_people-table/static/css/main.51ae3472.chunk.css"
+  },
+  {
+    "revision": "054c0c05cbf76aa6fbfd",
+    "url": "/react_people-table/static/js/2.88bdd582.chunk.js"
+  },
+  {
+    "revision": "4bba454c93533744ed05",
+    "url": "/react_people-table/static/js/main.a4cb79eb.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
